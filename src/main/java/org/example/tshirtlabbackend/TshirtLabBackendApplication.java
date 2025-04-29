0f2661958455410cdc19d143408d8463b3cb6dba
Maksim Madžar <madzarmaksim@gmail.com>
2025-04-29 21:50:18 +0200

    init

---
+++ src/main/java/org/example/tshirtlabbackend/TshirtLabBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tshirtlabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TshirtLabBackendApplication.class, args);
+    }
+
+}
