4fb15bf0b71f963fba063ecc98023ecddf3f09f9
Maksim Madžar <madzarmaksim@gmail.com>
2025-06-03 10:00:59 +0200

    feat: add AWS credentials configuration for S3 client and update .gitignore

--- src/main/java/org/example/tshirtlabbackend/config/S3Config.java
+++ src/main/java/org/example/tshirtlabbackend/config/S3Config.java
@@ -3,8 +3,10 @@ package org.example.tshirtlabbackend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
@@ -21,11 +23,20 @@ public class S3Config {
     @Value("${app.aws.endpoint:}")
     private String endpoint;
 
+    @Value("${app.aws.access-key-id}")
+    private String awsKey;
+
+    @Value("${app.aws.secret-access-key}")
+    private String awsSecret;
+
+
     @Bean
     public S3Client s3Client() {
         S3ClientBuilder builder = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create());
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(awsKey, awsSecret)
+                ));
 
         if (!endpoint.isBlank()) {
             builder.endpointOverride(URI.create(endpoint));
@@ -37,7 +48,9 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         S3Presigner.Builder builder = S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create());
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(awsKey, awsSecret)
+                ));
 
         if (!endpoint.isBlank()) {
             builder.endpointOverride(URI.create(endpoint));
